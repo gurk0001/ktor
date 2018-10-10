294edf70a5a892a16ff1e3841e0c0106ee0fc64b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-10-10 07:59:29 +0300

    fixup! Fix js gradle config

--- ktor-client/ktor-client-js/test/io/ktor/client/engine/js/test/RequestTest.kt
+++ ktor-client/ktor-client-js/test/io/ktor/client/engine/js/test/RequestTest.kt
@@ -4,17 +4,16 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.response.*
 import io.ktor.http.*
-import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.*
 import kotlin.js.*
 import kotlin.test.*
 
 class RequestTest {
 
     @Test
-    fun simpleRequestTest(): Promise<Unit> = async {
+    fun simpleRequestTest(): Promise<Unit> = GlobalScope.async(Dispatchers.Default, CoroutineStart.DEFAULT) {
         val client = HttpClient()
-
-        val response: HttpResponse = client.get("https://google.com")
+        val response: HttpResponse = client.get("https://www.google.com")
 
         assertTrue(response.status.isSuccess())
         val text = response.readText()
